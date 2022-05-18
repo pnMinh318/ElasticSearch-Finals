017baf1eca145cda5c7f45b7c2d6e6e42c28b780
Minh <18110318@student.hcmute.edu.vn>
2022-05-18 09:20:46 +0700

    comments endpoint,login & register endpoint

---
+++ server/controllers/commentController.js
@@ -0,0 +1,45 @@
+import { client } from "../elastic.js";
+
+const getCommentByBlogId = async (req, res) => {
+  const id = req.params.id;
+  const documents = await client.search({
+    index: "post_comments",
+    query: {
+      match: {
+        blogId: id,
+      },
+    },
+  });
+
+  if (documents.hits.hits.length > 0) {
+    const data = documents.hits.hits[0]._source;
+    res.status(200).json({ documents: data });
+  } else {
+    res.status(404).json({ error: "COMMENTS NOT FOUND" });
+  }
+};
+
+const postComment = async (req, res) => {
+  const blogId = req.params.blogId;
+  const { content, userId } = req.body;
+  const bulkResponse = await client.bulk({
+    refresh: true,
+    operations: [
+      { index: { _index: "post_comments" } },
+      {
+        userId: userId,
+        blogId: blogId,
+        content: content,
+        createdAt: Date.now(),
+      },
+    ],
+  });
+
+  if (bulkResponse.errors) {
+    res.status(400).json({ error: bulkResponse });
+  } else {
+    res.status(200).json({ message: "INSERT COMMENT SUCCESSFUL" });
+  }
+};
+
+export { postComment, getCommentByBlogId };
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,51 +1,74 @@
 import { client } from "../elastic.js";
 
-// const login = async (req, res) => {
-//   const { username, password } = req.body;
-//   if (username && password) {
-//     const user = await client.search({
-//       username: username,
-//       password: password,
-//     });
-//     if (user) {
-//       //generate token
-//       res.status(200).json({ user });
-//     }
-//   }
-// };
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    const documents = await client.search({
+      index: "post_user",
+      query: {
+        match: {
+          username: username,
+        },
+      },
+    });
+    if (documents.hits.hits.length < 0) {
+      res.status(403).json({ message: "User not Existed" });
+    } else {
+      const userInfo = documents.hits.hits[0]._source;
+      if (userInfo.password === password)
+        res.status(200).json({
+          message: "User login successful",
+          user: {
+            id: documents.hits.hits[0]._id,
+            username: userInfo.username,
+            fullname: userInfo.fullname,
+          },
+        });
+      else {
+        res.status(401).json({ message: "Wrong password!" });
+      }
+    }
+  }
+};
 
-// const register = async (req, res) => {
-//   const { username, password } = req.body;
-//   if (username && password) {
-//     const usernameExisted = await client.search({
-//       username: username,
-//     });
-//     if (usernameExisted) {
-//       res.status(200).json({ message: "Username already Existed" });
-//     } else {
-//       const bulkResponse = await client.bulk({
-//         refresh: true,
-//         operations: [
-//           { index: { _index: "user" } },
-//           {
-//             username,
-//             password,
-//           },
-//         ],
-//       });
+const register = async (req, res) => {
+  const { username, password, fullname } = req.body;
+  if (username && password && fullname) {
+    const usernameExisted = await client.search({
+      index: "post_user",
+      query: {
+        match: {
+          username: username,
+        },
+      },
+    });
+    if (usernameExisted.hits.hits.length > 0) {
+      res.status(404).json({ message: "Username already Existed" });
+    } else {
+      const bulkResponse = await client.bulk({
+        refresh: true,
+        operations: [
+          { index: { _index: "post_user" } },
+          {
+            username,
+            password,
+            fullname,
+          },
+        ],
+      });
 
-//       if (bulkResponse.errors) {
-//         res.status(400).json({ message: "REGISTER FAILED" });
-//         console.log(bulkResponse);
-//         process.exit(1);
-//       } else {
-//         res.status(200).json({ message: "REGISTER SUCCESSFUL" });
-//       }
-//     }
-//   } else {
-//     res.status(404).json({ message: "Username or Password not valid!!!" });
-//   }
-// };
+      if (bulkResponse.errors) {
+        res.status(400).json({ message: "REGISTER FAILED" });
+      } else {
+        res.status(200).json({ message: "REGISTER SUCCESSFUL" });
+      }
+    }
+  } else {
+    res
+      .status(404)
+      .json({ message: "Username or Password or Fullname not valid!!!" });
+  }
+};
 
 // const updateUser = async (req, res) => {
 //   const { username, password } = req.body;
@@ -80,4 +103,4 @@ import { client } from "../elastic.js";
 //   }
 // };
 // const lockUser = async (req, res) => {};
-// export { register, login, updateUser, lockUser };
+export { register, login };
